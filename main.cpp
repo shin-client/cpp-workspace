84321736b281ccec57adb11c6da894952225fddb
shin-client <47554598+shin-client@users.noreply.github.com>
2024-10-04 05:38:30 +0000

    tét

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a = 10;
-    cout << a;
-    return 0;
+int main(){
+  int n, countChan = 0;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    if (n % i == 0 && i % 2 == 0) countChan++;
+  }
+  cout << "Co " << countChan << " uoc chan\n";
+  return 0;
 }
